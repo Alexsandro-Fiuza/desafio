60b0ef613eaab79546a9df23db70abccf1a64265
Alexsandro <alexxfiuza33@gmail.com>
2023-09-05 11:12:08 -0300

    Second Commit

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 //const dataatual2 = new Date()
 var data = window.prompt("Digite uma data no formato -> yyyy/mm/dd")
 var res = document.getElementById("res")
-var final = document.getElementById("fim")
-const dias = document.getElementById("days")
-const horas = document.getElementById("hours")
-const minutos = document.getElementById("minutes")
-const segundos = document.getElementById("seconds")
 
-if (data.length.value == 0) {
-  final.innerHTML = "<strong>Digite uma data acima!<strong>"
-} else {
-  final.innerHTML = ""
-}
+
+
 
 function contador() {
   const datareal = new Date(`${data}`)
@@ -24,18 +16,20 @@ function contador() {
   const minutos1 = Math.floor(segundostotais / 60) % 60
   const segundos1 = Math.floor(segundostotais) % 60
 
-  res.style.width = "170px"
-
-  dias.innerHTML = `<strong>${dias1}d:<strong>`
-  horas.innerHTML = `<strong>${horas1}h:<strong>`
-  minutos.innerHTML = `<strong>${minutos1}m:<strong>`
-  segundos.innerHTML = `<strong>${segundos1}s<strong>`
+  
+  const result =
+    `<strong>${dias1}d:<strong>` +
+    `<strong>${horas1}h:<strong>` +
+    `<strong>${minutos1}m:<strong>` + `<strong>${segundos1}s<strong>`
+  //console.log(result)
+  res.innerHTML = result
 }
+
 
 function formatTime(time) {
   return time < 10 ? `0${time}` : time
 }
 
-contador()
+
 
 setInterval(contador, 1000)
